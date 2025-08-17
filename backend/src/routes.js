1243adbe82dde81fab166790302da65c76f282bb
Henry <henrykaua21@gmail.com>
2025-08-16 23:46:38 -0300

    feat: adiciona rota de mensagem

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,14 @@ router.get('/', (req, res) => {
   res.send('Testando rota!')
 })
 
+router.post('/mensagem', (req, res) => {
+  const { telefone, mensagem } = req.body;
+  console.log('Nova mensagem recebida');
+  console.log('Telefone/usuario', telefone);
+  console.log('mensagem', mensagem);
+
+  res.status(200).json({ message: 'mensagem salva com sucesso' })
+})
+
 
 export default router;
