6e5cbdddeacd618982187af3c712f7cd66f989ea
Henry <henrykaua21@gmail.com>
2025-08-18 23:23:47 -0300

    feat: adiciona comando do bot e iniciando o bot, com comando que mostra os comandos disponiveis e comando para criar conta fazendo requisicao para api /usuarios

--- backend/src/bot.js
+++ backend/src/bot.js
@@ -4,25 +4,50 @@ import 'dotenv/config'
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on('text', async (ctx) => {
-  const mensagem = ctx.message.text;
-  const telefone = ctx.message.from.username || ctx.message.from.id;
+bot.command('help', (ctx) => {
+  ctx.reply(
+    `Comandos disponíveis:
+/help - Lista todos os comandos
+/salvar_conta - Mande informações da sua conta para que possamos salvar(ex: Conta de Luz)
+/minhas_contas - Visualizar suas contas cadastradas
+/participantes - Gerenciar participantes de uma conta
+/pegar_toke - Gere token de suas contas para compartilhar com outras pessoas e enviar notificações`
+  );
+});
 
+bot.command('criar_conta', async (ctx) => {
+  const telegramId = ctx.from.id;
+  const nome = ctx.from.first_name;
+  const username = ctx.from.username;
   try {
-    await axios.post(`${process.env.API_URL}/mensagem`, {
-      telefone,
-      mensagem
+    await axios.post(`${process.env.API_URL}/usuarios`, {
+      telegramId,
+      nome,
+      username
     });
-    ctx.reply('Mensagem recebida!');
+    ctx.reply(`Olá ${nome}! Sua conta foi criada com sucesso.`);
   } catch (error) {
-    console.error("Erro ao enviar mensagem:", error.message);
-    ctx.reply('Erro ao enviar mensagem');
+    console.error('Erro ao criar usuário:', error.response?.data || error.message);
+    ctx.reply('Ocorreu um erro ao criar sua conta tente novamente.');
   }
 });
 
+bot.on('text', async (ctx) => {
+  ctx.reply(
+    `Olá ${ctx.from.first_name}! 👋
+Eu sou seu assistente de contas compartilhadas.
+Você pode usar os seguintes comandos:
+
+/help - Lista todos os comandos
+/salvar_conta - Mande informações da sua conta para que possamos salvar(ex: Conta de Luz)
+/minhas_contas - Visualizar suas contas cadastradas
+/participantes - Gerenciar participantes de uma conta
+/pegar_toke - Gere token de suas contas para compartilhar com outras pessoas e enviar notificações`
+  );
+});
+
 bot.launch().then(() => {
-  console.log("Bot iniciado com sucesso!");
-  bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, 'ola');
+  console.log('Bot iniciado com sucesso!');
 }).catch(err => {
-  console.error("Erro ao iniciar o bot:", err.message);
+  console.error('Erro ao iniciar o bot:', err.message);
 });
