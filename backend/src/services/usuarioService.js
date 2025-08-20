b16ee05205c8e6ddc7106e547daaf641cc8de950
Henry <henrykaua21@gmail.com>
2025-08-19 22:50:45 -0300

    feat: adiciona services que trata informacoes vindo do bot como valor e no service ele passa por um tratamento, alem das datas e uma pesquisa para saber se o usuario tem conta cadastrada, passada por isso envia informacoes ao repositories para que salve no banco de dados

--- backend/src/services/usuarioService.js
+++ backend/src/services/usuarioService.js
@@ -1,4 +1,5 @@
 import { usuarioRepository } from "../repositories/usuarioRepository.js";
+import { parseDateUser, validateFutureDate } from "../utils/dateTime.js";
 
 export const usuarioService = {
     async createUser({telegramId, nome, telefone}) {
@@ -7,5 +8,32 @@ export const usuarioService = {
             throw new Error('Usuário já cadastrado com esse Telegram ID');
         }
         return await usuarioRepository.createUser({telegramId, nome, telefone});
+    },
+
+    async createCount({ nome, dataDeVencimento, valorTotal, chavePix, telegramId }) {
+        const usuario = await usuarioRepository.searchByTelegramId(telegramId);
+        if (!usuario) {
+            throw new Error('Usuário não encontrado.');
+        }
+        const dataConvertida = parseDateUser(dataDeVencimento);
+        validateFutureDate(dataConvertida);
+        const valorConvertido = parseFloat(valorTotal.toString().replace(',', '.'));
+        if (valorConvertido <= 0) {
+            throw new Error('O valor da conta deve ser maior que zero');
+        }
+        try {
+            return await usuarioRepository.createCount({
+                nome,
+                dataDeVencimento: dataConvertida,
+                valorTotal: valorConvertido,
+                chavePix,
+                criadorId: usuario.id,
+            });
+        } catch (error) {
+            if (error.code === 'P2002') {
+                throw new Error('Conta já existe com esses dados.');
+            }
+            throw new Error('Erro ao criar a conta.');
+        }
     },
 };
