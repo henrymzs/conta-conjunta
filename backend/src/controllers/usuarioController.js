5de40174797b41da7348e462718879373cd10daf
Henry <henrykaua21@gmail.com>
2025-08-19 22:46:25 -0300

    feat adiciona controller para adicionar conta no banco de dados, validando dados importantes antes de ser enviado para o service

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -10,4 +10,17 @@ export const usuarioController = {
             return res.status(400).json({ erro: error.message });
         }
     },
+
+    async createCount(req, res) {
+        try {
+            const { nome, dataDeVencimento, valorTotal, chavePix, telegramId } = req.body;
+            if (!nome || !dataDeVencimento || !valorTotal) {
+                return res.status(400).json({ error: 'Campos obrigatórios não preenchidos.' });
+            }
+            const conta = await usuarioService.createCount({ nome, dataDeVencimento, valorTotal, chavePix, telegramId });
+            return res.status(201).json(conta);
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
+        }
+    }
 };
